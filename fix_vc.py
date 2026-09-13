77341b021f29e18a8c506279aebbb2e1cbe91fec
Ayush <hyt319936@gmail.com>
2026-09-13 13:09:43 +0530

    Fix installer requiring 32-bit VC++ runtime on 64-bit systems

---
+++ fix_vc.py
@@ -0,0 +1,29 @@
+﻿import os
+
+fp = r'C:\Sentinel-Agent\Lts\v7\obylon-7.0.5\installer\obylon-setup.iss'
+with open(fp, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Only require the architecture-appropriate VC runtime
+old_checks = '''    if CurPageID = wpWelcome then
+    begin
+      NeedsVC64 := not VCReady(True);
+      NeedsVC32 := not VCReady(False);'''
+
+new_checks = '''    if CurPageID = wpWelcome then
+    begin
+      if IsWin64 then
+      begin
+        NeedsVC64 := not VCReady(True);
+        NeedsVC32 := False;
+      end
+      else
+      begin
+        NeedsVC64 := False;
+        NeedsVC32 := not VCReady(False);
+      end;'''
+
+content = content.replace(old_checks, new_checks)
+
+with open(fp, 'w', encoding='utf-8') as f:
+    f.write(content)
